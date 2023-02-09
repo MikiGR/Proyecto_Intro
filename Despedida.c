fdda5052167d3ac7b31688634c2b9cc0f7a98509
pabloab2 <pabloab0011@yahoo.es>
2023-02-09 12:22:17 +0100

    dedicado a ranqui

--- Despedida.c
+++ Despedida.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main() {
+  int edad_mental_ranqui = 2;
     printf( "\n   Oigan si quieren que me vaya me lo decís...." );
   printf( "\n   Esto definitivamente es una despedida, adiós." );
   printf("HOlaaaaaa ");
+  printf("SI OS SALE ESTO AVISAME SOY ALONSO");
+  printf("\n\tLa edad mental de ranqui es : %d years", edad_mental_ranqui);
   return 0;
 }
