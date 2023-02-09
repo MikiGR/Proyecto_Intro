4b2bcc9d5444160bf22e1844ac11e1c50bbf812a
pablobarrancocespedes <pablo.barranco.cespedes@gmail.com>
2023-02-09 12:26:18 +0100

    Update Despedida.c

--- Despedida.c
+++ Despedida.c
@@ -2,12 +2,13 @@
 
 int main() {
   int edad_mental_ranqui = 2;
-  double cm_nepe_ranqui = 0.1;
+  double hectometros_nepe_ranqui = 0.1;
     printf( "\n   Oigan si quieren que me vaya me lo decís...." );
   printf( "\n   Esto definitivamente es una despedida, adiós." );
   printf("HOlaaaaaa ");
   printf("SI OS SALE ESTO AVISAME SOY ALONSO");
   printf("\n\tLa edad mental de ranqui es : %d years", edad_mental_ranqui);
-  printf("\n\tA ranqui le mide: %f years", cm_nepe_ranqui);
+  printf("\n\tA ranqui le mide: %f years", hectometros_nepe_ranqui);
+  printf("Oigan esto no es gracioso eh");
   return 0;
 }
