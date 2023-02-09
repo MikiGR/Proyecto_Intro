1aaa00db4fb8c9edfa673a2891f8b888516040b4
pabloleonn <pablolv.8.plv@gmail.com>
2023-02-09 12:23:53 +0100

    Update Despedida.c

--- Despedida.c
+++ Despedida.c
@@ -2,10 +2,12 @@
 
 int main() {
   int edad_mental_ranqui = 2;
+  double cm_nepe_ranqui = 0.1;
     printf( "\n   Oigan si quieren que me vaya me lo decís...." );
   printf( "\n   Esto definitivamente es una despedida, adiós." );
   printf("HOlaaaaaa ");
   printf("SI OS SALE ESTO AVISAME SOY ALONSO");
   printf("\n\tLa edad mental de ranqui es : %d years", edad_mental_ranqui);
+  printf("\n\tA ranqui le mide: %f years", cm_nepe_ranqui);
   return 0;
 }
